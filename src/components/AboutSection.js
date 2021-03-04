da53ebe8fcb9bc5dcf3c2d76495bce5796466448
sidwp16 <sidlaurenzortega@gmail.com>
2021-03-04 22:36:04 +0800

    12. Framer Motions

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,25 +2,43 @@ import React from "react";
 import home1 from "../img/home1.png";
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../style";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.75 },
+    },
+  };
   return (
     <About>
       <Description>
-        <Hide>
-          <div className="title">
-            <h2>We work to make</h2>
-          </div>
-        </Hide>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
+          <Hide>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+          </Hide>
 
-        <Hide>
-          <h2>
-            your <span>dreams</span> come
-          </h2>
-        </Hide>
-        <Hide>
-          <h2>true.</h2>
-        </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
+          </Hide>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professional with amazing skills
